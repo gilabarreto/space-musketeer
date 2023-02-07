8d30d4b8480623b97946207f3a8344da5d0d6914
Victor Gila Barreto <gilabarreto@gmail.com>
2023-02-06 22:38:13 -0700

    Create ship with canva

--- space.js
+++ space.js
@@ -8,9 +8,36 @@ let boardWidth = tileSize * columns; // 32 * 16
 let boardHeight = tileSize * rows; // 32 * 16
 let context;
 
-window.onload = function() {
+// Ship
+
+let shipWidth = tileSize * 2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns / 2 - tileSize;
+let shipY = tileSize * rows - tileSize * 2;
+
+let ship = {
+  x: shipX,
+  y: shipY,
+  width: shipWidth,
+  heigth: shipHeight
+}
+
+let shipImg;
+
+window.onload = function () {
   board = document.getElementById("board");
   board.width = boardWidth;
   board.height = boardHeight;
   context = board.getContext("2d"); // use for drawing on the board
+
+  // draw initial ship
+  // context.fillStyle = "green";
+  // context.fillRect(ship.x, ship.y, ship.width, ship.heigth);
+
+  // load images
+  shipImg = new Image();
+  shipImg.src = "./musketeer.png"
+  shipImg.onload = function () {
+    context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.heigth)
+  }
 }
